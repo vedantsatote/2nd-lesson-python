bad91e60e320db4ebfad68819441e1af06e9c5c9
vedantsatote <satoteonline@gmail.com>
2024-08-08 23:21:46 +0530

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,3 @@
+#write a program to input user's first name & print its length.
+name= input("enter your name :")
+print("lenght of your name is", len(name))
---
+++ signal conditional statement.py
@@ -0,0 +1,11 @@
+light = input("color of light :")
+if (light == "red"):
+  print("stop")
+elif(light == "green"):
+  print("go")
+elif(light == "yellow"):
+  print("look")
+else:
+  print("light is broken")
+
+print ("end of code")
